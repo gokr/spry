4a9861f2f08c8121d8d50699f4291afacaa48084
Göran Krampe <goran@3dicc.com>
2015-09-07 18:25:37 +0200

    Silly example of running a Ni script in a Nim program

--- factorial.nim
+++ factorial.nim
@@ -2,10 +2,6 @@
 import ni
 
 discard newInterpreter().eval """
-  f: 1 n: 20
-  factorial: [ifelse (n > 1) [f: (f * n) n: (n - 1) do factorial] [f]]
-  loop 100000 [
-    f: 1 n: 20
-    do factorial]
-  echo f
-  """
+  factorial = [ifelse (:n > 1) [n * factorial(n - 1)] [1]]
+  echo(factorial 12)
+"""
