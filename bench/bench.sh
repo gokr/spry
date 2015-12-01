233f61b42e128c4789467736aac230712833a3d6
Göran Krampe <goran.krampe@evothings.com>
2015-12-01 11:36:23 +0100

    Added GC v2 to bench, it's a tad faster

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,3 +1,8 @@
 nim c -d:release --out:$PWD/ni ../src/ni.nim
-echo "Ni:"
+nim c -d:release --gc:v2 --out:$PWD/niv2 ../src/ni.nim
+
+echo
+echo "Ni with default gc:"
 time ./ni factorial.ni
+echo "Ni with default gc v2:"
+time ./niv2 factorial.ni
