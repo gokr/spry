7c44c528d40d3ae6e8f811efaba52d805bfd1cec
Göran Krampe <goran.krampe@evothings.com>
2016-08-19 12:24:05 +0200

    A memfile module added

---
+++ src/modules/sprymemfile.nim
@@ -0,0 +1,50 @@
+import spryvm
+
+import memfiles
+
+type MemfileNode* = ref object of Value
+  memfile*: Memfile
+
+method eval*(self: MemfileNode, spry: Interpreter): Node =
+  self
+
+# Spry Memfile module
+proc addMemfile*(spry: Interpreter) =
+  # IO
+  nimPrim("openMemfile", false):
+    let path = StringVal(evalArg(spry)).value
+    result = MemfileNode(memfile: memfiles.open(path))
+  nimPrim("closeMemfile", true):
+    let node = MemfileNode(evalArgInfix(spry))
+    memfiles.close(node.memfile)
+  nimPrim("readLines", false):
+    let path = StringVal(evalArg(spry)).value
+    var memfile = memfiles.open(path)
+    result = newBlok()
+    for line in lines(memfile):
+      Blok(result).add(StringVal(value: string(line)))
+    memfiles.close(memfile)
+  nimPrim("linesDo:", true):
+    result = MemfileNode(evalArgInfix(spry))
+    let memfile = MemfileNode(result).memfile
+    let blk = Blok(evalArg(spry))
+    # Ugly hack for now, we trick the activation into holding each in pos 0
+    let current = spry.currentActivation
+    let orig = current.body.nodes[0]
+    let oldpos = current.pos
+    current.pos = 0
+    let activation = newActivation(blk)
+    for line in lines(memfile):
+      current.body.nodes[0] = StringVal(value: string(line))
+      discard activation.eval(spry)
+      activation.reset()
+      current.pos = 0
+      # Or else non local returns don't work :)
+      if current.returned:
+        # Reset our trick
+        current.body.nodes[0] = orig
+        current.pos = oldpos
+        return current.last
+    # Reset our trick
+    current.body.nodes[0] = orig
+    current.pos = oldpos
