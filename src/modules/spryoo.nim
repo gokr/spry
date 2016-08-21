f0468af9064a165fe46c16e1634bef6a5de5823a
Göran Krampe <goran.krampe@evothings.com>
2016-08-21 19:57:03 +0200

    OO tweaks

--- src/modules/spryoo.nim
+++ src/modules/spryoo.nim
@@ -5,29 +5,32 @@ type
   # nodes hold Funk instances
   PolyMeth* = ref object of Meth
 
-proc newPolyMeth*(funks: Blok, parent: Activation): Funk =
-  PolyMeth(nodes: funks.nodes, parent: parent)
+proc newPolyMeth*(methods: Blok, parent: Activation): Funk =
+  PolyMeth(nodes: methods.nodes, parent: parent)
 
-proc polymeth*(funks: Blok, spry: Interpreter): Node =
-  newPolyMeth(funks, spry.currentActivation)
+proc polymeth*(methods: Blok, spry: Interpreter): Node =
+  newPolyMeth(methods, spry.currentActivation)
 
 method `$`*(self: PolyMeth): string =
   return "polymethod [" & $self.nodes & "]"
 
-method eval(self: PolyMeth, spry: Interpreter): Node =
+method eval*(self: PolyMeth, spry: Interpreter): Node =
   let receiver = evalArgInfix(spry)
   if receiver.isNil:
     return spry.nilVal
   let tags = receiver.tags
   if tags.isNil:
     return spry.nilVal
-  let tagNodes = tags.nodes
-  if tagNodes.isNil:
+  let nodeTags = tags.nodes
+  if nodeTags.isNil:
     return spry.nilVal
   for n in self.nodes:
-    let fun = Funk(n)
-    if any(tagNodes, proc (x: Node): bool = return fun.tags.nodes.contains(x)):
-      return fun.eval(spry)
+    let funTags = Funk(n).tags.nodes
+    for nt in nodeTags:
+      if funTags.contains(nt):
+        return Funk(n).eval(spry)
+    #if any(nodeTags, proc (x: Node): bool = return fun.tags.nodes.contains(x)):
+    #  return fun.eval(spry)
   return spry.nilVal
 
 # Spry OO module
