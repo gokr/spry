af10b38be309df801a29a84389de15439d3b2c74
Göran Krampe <goran.krampe@evothings.com>
2016-08-30 00:19:35 +0200

    Tiny.sh added

--- bench/factorial.sh
+++ bench/factorial.sh
@@ -3,13 +3,11 @@ nim c -d:release --out:$PWD/spry ../src/spry.nim
 nim c -d:release --cc:clang --out:$PWD/spryclang ../src/spry.nim
 
 echo
-echo "Spry with gcc:"
+echo "Spry with gcc, factorial.sy"
 /usr/bin/time -v ./spry factorial.sy
 echo
-echo "Spry with clang:"
+echo "Spry with clang, factorial.sy"
 /usr/bin/time -v ./spryclang factorial.sy
 echo
 echo "Spry using nimath fac primitive function:"
 /usr/bin/time -v ./spry fac.sy
-
-
