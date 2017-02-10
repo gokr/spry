04a8744b33c5907d46258efdbb3ce07d570637b7
Göran Krampe <goran.krampe@evothings.com>
2017-02-10 08:12:27 +0100

    Optional use of upx in makeide.sh

--- src/makeide.sh
+++ src/makeide.sh
@@ -38,5 +38,15 @@ EOF
 
 # Through experiments this builds libui statically linked
 nim --verbosity:2 -d:release --dynlibOverride:ui  --passL:"-rdynamic ../lib/libui.a -lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0" c ide
+
+# Strip
 strip -s ide
-upx --best ide
+
+# Use upx if we have it
+command -v upxx >/dev/null 2>&1 && {
+  upx --best ide
+}
+
+# Done
+echo "Produced: ./ide"
+file ide
