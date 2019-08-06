87bcf520b1649de0ed187a700671015185e028e5
Göran Krampe <goran.krampe@gmail.com>
2019-08-07 00:26:24 +0200

    Restructure samples and made ide.sy work

--- samples/gui/ide/makeide.sh
+++ samples/gui/ide/makeide.sh
@@ -43,9 +43,6 @@ cat << EOF >> ./ide.nim
 ]""")
 EOF
 
-# Through experiments this builds libui statically linked
-#nim --verbosity:2 -d:release --dynlibOverride:ui  --passL:"-rdynamic ../lib/libui.a -lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0" c ide
-
 nim c ide
 
 # Strip
